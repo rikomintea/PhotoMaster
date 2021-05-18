2025625591a1cb7bcfadf8a7a8b3b500551a3906
Riko Yasuda <rikomintea1827@gmail.com>
2021-05-18 15:44:13 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by スマート・ナビ on 2021/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
